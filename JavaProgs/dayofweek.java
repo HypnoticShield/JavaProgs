1a9f2d92fac7f1d07d1244e11ea6e3882730d2bb
Arnav Sharma <arnav.2005.sharma@gmail.com>
2025-01-26 18:50:26 +0530

    some more

---
+++ JavaProgs/dayofweek.java
@@ -0,0 +1,36 @@
+//Create a program that accepts a number (1–7) and displays the corresponding day of the week using a switch statement.
+
+import java.util.Scanner;
+
+public class dayofweek{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number (1-7): ");
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+}
